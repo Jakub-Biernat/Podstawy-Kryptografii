258e5048243000f08184c0833ca99dcd4a605f45
Jakub-Biernat <k.biernat@vp.pl>
2026-04-03 14:50:24 +0200

    Add plots

--- Lab3/funkcje_skrotu.py
+++ Lab3/funkcje_skrotu.py
@@ -3,40 +3,6 @@ import sys
 import time
 import matplotlib.pyplot as plt
 
-
-def generate_and_compare_hashes(text):
-    functions = {
-        'MD5': hashlib.md5(),
-        'SHA1': hashlib.sha1(),
-        'SHA2_224': hashlib.sha224(),
-        'SHA2_256': hashlib.sha256(),
-        'SHA2_384': hashlib.sha384(),
-        'SHA2_512': hashlib.sha512(),
-        'SHA3_224': hashlib.sha3_224(),
-        'SHA3_256': hashlib.sha3_256(),
-        'SHA3_384': hashlib.sha3_384(),
-        'SHA3_512': hashlib.sha3_512(),
-    }
-
-    input_bits = len(text.encode()) * 8
-    print("########################################################## TEST DANYCH WEJSCIOWYCH DLUGOSCI " +
-          str(input_bits) + " BITOW ##########################################################")
-    print("Kodowany tekst: " + text)
-    print()
-
-    hashes = {}
-    for name, function in functions.items():
-        start = time.time()
-        function.update(text.encode())
-        hash = function.hexdigest()
-        stop = time.time()
-        hashes[name] = hash
-        output_bits = len(hash) * 4
-        print("Wygenerowany hash przy uzyciu algorytmu " + name + ": " + hashes[name])
-        print("Czas dzialania tego algorytmu: " + str(stop - start) + "sekund")
-        print("Dlugosc ciagu wyjsciowego: " + str(output_bits) + " bitow")
-        print()
-
 def generate_and_compare_hashes(texts):
     functions = {
         'MD5': hashlib.md5(),
@@ -68,12 +34,38 @@ def generate_and_compare_hashes(texts):
             times[name].append(stop - start)
             hash_bits[name].append(len(hash) * 4)
 
+    plt.figure(figsize=(12, 6))
 
+    plt.subplot(1, 2, 1)
+    for name in functions:
+        plt.plot(input_bits, hash_bits[name], label=name)
+
+    plt.xlabel("Długość danych wejściowych (bity)")
+    plt.ylabel("Długość hasha (bity)")
+    plt.title("Długość hasha od długości danych wejściowych")
+    plt.legend()
+    plt.grid()
+
+    plt.subplot(1, 2, 2)
+    for name in functions:
+        plt.plot(input_bits, times[name], label=name)
+    plt.xlabel("Długość danych wejściowych (bity)")
+    plt.ylabel("Czas wykonania (sekundy)")
+    plt.title("Czas wykonania od długości danych wejściowych")
+    plt.legend()
+    plt.grid()
+
+    plt.tight_layout()
+    plt.show()
 
 def main():
-    textfile = sys.argv[1]
-    with open(textfile, 'r') as f:
-        text = f.read()
-        generate_and_compare_hashes(text)
+    texts = [
+        "a",
+        "a" * 10,
+        "a" * 100,
+        "a" * 1000,
+    ]
+
+    generate_and_compare_hashes(texts)
 
 main()
