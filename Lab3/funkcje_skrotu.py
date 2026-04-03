4e7455d7b3926691086e14c0c417c1313919aad1
Jakub-Biernat <k.biernat@vp.pl>
2026-04-03 10:44:44 +0200

    Add comparing hash algorithms

--- Lab3/funkcje_skrotu.py
+++ Lab3/funkcje_skrotu.py
@@ -1,29 +1,43 @@
 import hashlib
 import sys
+import time
 
 
-def generate_hash(text):
+def generate_and_compare_hashes(text):
     functions = {
-        'md5': hashlib.md5(),
-        'sha1': hashlib.sha1(),
-        'sha224': hashlib.sha224(),
-        'sha256': hashlib.sha256(),
-        'sha384': hashlib.sha384(),
-        'sha512': hashlib.sha512(),
-        'sha3_224': hashlib.sha3_224(),
-        'sha3_256': hashlib.sha3_256(),
-        'sha3_384': hashlib.sha3_384(),
-        'sha3_512': hashlib.sha3_512(),
+        'MD5': hashlib.md5(),
+        'SHA1': hashlib.sha1(),
+        'SHA2_224': hashlib.sha224(),
+        'SHA2_256': hashlib.sha256(),
+        'SHA2_384': hashlib.sha384(),
+        'SHA2_512': hashlib.sha512(),
+        'SHA3_224': hashlib.sha3_224(),
+        'SHA3_256': hashlib.sha3_256(),
+        'SHA3_384': hashlib.sha3_384(),
+        'SHA3_512': hashlib.sha3_512(),
     }
 
+    input_bits = len(text.encode()) * 8
+    print("########################################################## TEST DANYCH WEJSCIOWYCH DLUGOSCI " +
+          str(input_bits) + " BITOW ##########################################################")
+
+    hashes = {}
     for name, function in functions.items():
+        start = time.time()
         function.update(text.encode())
-        print("Wygenerowany hash przy uzyciu algorytmu " + name + ":" + function.hexdigest())
+        hash = function.hexdigest()
+        stop = time.time()
+        hashes[name] = hash
+        output_bits = len(hash) * 4
+        print("Wygenerowany hash przy uzyciu algorytmu " + name + ": " + hashes[name])
+        print("Czas dzialania tego algorytmu: " + str(stop - start) + "sekund")
+        print("Dlugosc ciagu wyjsciowego: " + str(output_bits) + " bitow")
+        print()
 
 def main():
     textfile = sys.argv[1]
     with open(textfile, 'r') as f:
         text = f.read()
-        generate_hash(text)
+        generate_and_compare_hashes(text)
 
 main()
