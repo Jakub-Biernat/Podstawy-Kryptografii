6240c6cf2cbf55e5b783a4ee200befcb8cc9f0d3
Jakub-Biernat <k.biernat@vp.pl>
2026-04-03 11:04:00 +0200

    Add plots (not finished)

--- Lab3/funkcje_skrotu.py
+++ Lab3/funkcje_skrotu.py
@@ -1,6 +1,7 @@
 import hashlib
 import sys
 import time
+import matplotlib.pyplot as plt
 
 
 def generate_and_compare_hashes(text):
@@ -35,6 +36,39 @@ def generate_and_compare_hashes(text):
         print("Czas dzialania tego algorytmu: " + str(stop - start) + "sekund")
         print("Dlugosc ciagu wyjsciowego: " + str(output_bits) + " bitow")
         print()
+
+def generate_and_compare_hashes(texts):
+    functions = {
+        'MD5': hashlib.md5(),
+        'SHA1': hashlib.sha1(),
+        'SHA2_224': hashlib.sha224(),
+        'SHA2_256': hashlib.sha256(),
+        'SHA2_384': hashlib.sha384(),
+        'SHA2_512': hashlib.sha512(),
+        'SHA3_224': hashlib.sha3_224(),
+        'SHA3_256': hashlib.sha3_256(),
+        'SHA3_384': hashlib.sha3_384(),
+        'SHA3_512': hashlib.sha3_512(),
+    }
+
+    input_bits = []
+    times = {name: [] for name in functions}
+    hash_bits = {name: [] for name in functions}
+    hashes = {}
+
+    for text in texts:
+        input_bits.append(len(text.encode()) * 8)
+        for name, function in functions.items():
+            start = time.perf_counter()
+            function.update(text.encode())
+            hash = function.hexdigest()
+            stop = time.perf_counter()
+
+            hashes[name] = hash
+            times[name].append(stop - start)
+            hash_bits[name].append(len(hash) * 4)
+
+
 
 def main():
     textfile = sys.argv[1]
