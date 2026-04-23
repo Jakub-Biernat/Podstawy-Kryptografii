3f75586c2add5bd669a8653db59356d0f027c2f1
Jakub Biernat <k.biernat@vp.pl>
2026-04-23 22:34:02 +0200

    Add Podpunkt 2 to Lab4

--- Lab4/szyfry_blokowe.py
+++ Lab4/szyfry_blokowe.py
@@ -1,7 +1,16 @@
+from secrets import choice
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import time
 import matplotlib.pyplot as plt
+
+MODES = {
+    "ECB": AES.MODE_ECB,
+    "CBC": AES.MODE_CBC,
+    "CFB": AES.MODE_CFB,
+    "OFB": AES.MODE_OFB,
+    "CTR": AES.MODE_CTR
+}
 
 def add_padding(text):
     text = text.encode()
@@ -18,7 +27,7 @@ def generate_cipher(mode, key):
 
     return cipher
 
-def generate_ciphertext(cipher, mode, text):
+def generate_ciphertext(cipher, text):
     return cipher.encrypt(text)
 
 def generate_decipher(mode, key, cipher):
@@ -35,26 +44,18 @@ def generate_decrypted(decipher, ciphertext):
     return decipher.decrypt(ciphertext)
 
 def compare_modes(texts):
-    modes = {
-        "ECB": AES.MODE_ECB,
-        "CBC": AES.MODE_CBC,
-        "CFB": AES.MODE_CFB,
-        "OFB": AES.MODE_OFB,
-        "CTR": AES.MODE_CTR
-    }
-
     key = get_random_bytes(16)
     input_bits = []
-    enc_times = {name: [] for name in modes.keys()}
-    dec_times = {name: [] for name in modes.keys()}
+    enc_times = {name: [] for name in MODES.keys()}
+    dec_times = {name: [] for name in MODES.keys()}
 
     for text in texts:
         text = add_padding(text)
-        input_bits.append(len(text))
-        for mode_name, mode in modes.items():
+        input_bits.append(len(text) * 8)
+        for mode_name, mode in MODES.items():
             cipher = generate_cipher(mode, key)
             start_enc = time.perf_counter()
-            ciphertext = generate_ciphertext(cipher, mode, text)
+            ciphertext = generate_ciphertext(cipher, text)
             stop_enc = time.perf_counter()
             enc_times[mode_name].append(stop_enc - start_enc)
 
@@ -67,7 +68,7 @@ def compare_modes(texts):
     plt.figure(figsize=(12, 6))
 
     ax1 = plt.subplot(1, 2, 1)
-    for mode in modes.keys():
+    for mode in MODES.keys():
         ax1.plot(input_bits, enc_times[mode], label=mode)
 
     ax1.set_xlabel("Długość danych wejściowych (bity)")
@@ -78,7 +79,7 @@ def compare_modes(texts):
     ax1.ticklabel_format(style='plain', axis='x')
 
     ax2 = plt.subplot(1, 2, 2)
-    for mode in modes.keys():
+    for mode in MODES.keys():
         ax2.plot(input_bits, dec_times[mode], label=mode)
 
     ax2.set_xlabel("Długość danych wejściowych (bity)")
@@ -94,9 +95,50 @@ def compare_modes(texts):
 
     return
 
+def flip_bit(text):
+    byte_arr = bytearray(text)
+    byte_index = choice(range(len(byte_arr)))
+    bit_index = choice(range(8))
+    byte_arr[byte_index] ^= (1 << bit_index)
+    return bytes(byte_arr)
+
+def byte_difference(originaltext, decrypted):
+    length = min(len(originaltext), len(decrypted))
+    return sum(1 for i in range(length) if originaltext[i] != decrypted[i])
+
+def test_modes_error_propagation():
+    text = "Litwo! Ojczyzno moja! ty jestes jak zdrowie. Ile cie trzeba cenic, ten tylko sie dowie. Kto cie stracil."
+    print("Test propagacji błędu")
+    print(f"Oryginalny tekst: {text}")
+    key = get_random_bytes(16)
+
+    padded_text = add_padding(text)
+    for mode_name, mode in MODES.items():
+        cipher = generate_cipher(mode, key)
+        ciphertext = generate_ciphertext(cipher, padded_text)
+
+        corrupted = flip_bit(ciphertext)
+
+        decipher = generate_decipher(mode, key, cipher)
+        decrypted = generate_decrypted(decipher, corrupted)
+
+        byte_diff = byte_difference(padded_text, decrypted)
+        decrypted = decrypted.decode('utf-8', errors='ignore')
+        print(f"Propagacja błędu {mode_name}")
+        print(f"Odszyfrowany tekst: {decrypted}")
+        if byte_diff == 1:
+            print(f"{mode_name}: 1 bajt został zmieniony, brak propagacji na inne bajty")
+        elif byte_diff <= 16:
+            print(f"{mode_name}: 1 blok uszkodzony, zmianie uległo {byte_diff} bajtów")
+        else:
+            print(f"{mode_name}: Propagacja błędu, zmianie uległo {byte_diff} bajtów")
+
 def main():
     #Podpunkt 1
     texts = ["a" * i for i in range(100000, 1000000, 100000)]
     compare_modes(texts)
+
+    #Podpunkt 2
+    test_modes_error_propagation()
 
 main()
